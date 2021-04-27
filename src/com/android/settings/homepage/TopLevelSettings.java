0c3e5269c6ba3d41a9147d83c73b5ec05583caf3
Elluzion <dyrex2004@gmail.com>
2021-04-27 12:52:39 +0530

    Settings: Remove overscroll glow on homepage
    We have spring overscroll now

--- src/com/android/settings/homepage/TopLevelSettings.java
+++ src/com/android/settings/homepage/TopLevelSettings.java
@@ -24,10 +24,11 @@ import android.content.Context;
 import android.os.Bundle;
 import android.os.UserHandle;
 import android.provider.Settings;
-
+import android.view.View;
 import androidx.fragment.app.Fragment;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.android.settings.R;
 import com.android.settings.core.SubSettingLauncher;
@@ -66,6 +67,13 @@ public class TopLevelSettings extends DashboardFragment implements
            default:
                return R.xml.top_level_settings_stylish;
         }
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        final RecyclerView recyclerView = getView().findViewById(R.id.recycler_view);
+        recyclerView.setOverScrollMode(View.OVER_SCROLL_NEVER);
     }
 
     @Override
