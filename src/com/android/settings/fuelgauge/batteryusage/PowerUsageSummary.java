f6d09ee3c1365de422f2c5582a4a8a3c2f711d4b
spkal01 <kalligeross@gmail.com>
2022-12-11 15:31:00 +0200

    Revert "Settings: Add toggle to switch between AOSP/Google battery graphs"
    This reverts commit bd2b31d9fe0566881b99818c81cc6d31f105465c.

--- src/com/android/settings/fuelgauge/batteryusage/PowerUsageSummary.java
+++ src/com/android/settings/fuelgauge/batteryusage/PowerUsageSummary.java
@@ -25,8 +25,6 @@ import android.database.ContentObserver;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
-import android.os.UserHandle;
-import android.provider.Settings;
 import android.provider.Settings.Global;
 import androidx.preference.Preference;
 import android.util.Log;
@@ -124,7 +122,6 @@ public class PowerUsageSummary extends PowerUsageBase implements
         @Override
         public void onChange(boolean selfChange, Uri uri) {
             restartBatteryInfoLoader();
-            initPreference();
         }
     };
 
@@ -244,10 +241,6 @@ public class PowerUsageSummary extends PowerUsageBase implements
                 Global.getUriFor(Global.BATTERY_ESTIMATES_LAST_UPDATE_TIME),
                 false,
                 mSettingsObserver);
-        getContentResolver().registerContentObserver(
-                Settings.System.getUriFor("battery_24_hrs_stats"),
-                false,
-                mSettingsObserver);
     }
 
     @Override
@@ -335,10 +328,8 @@ public class PowerUsageSummary extends PowerUsageBase implements
     @VisibleForTesting
     void initPreference() {
         mBatteryUsagePreference = findPreference(KEY_BATTERY_USAGE);
-        boolean isChartGraphEnabled = Settings.System.getIntForUser(getContext().getContentResolver(),
-                "battery_24_hrs_stats", 0, UserHandle.USER_CURRENT) != 0;
         mBatteryUsagePreference.setSummary(
-                isChartGraphEnabled ?
+                mPowerFeatureProvider.isChartGraphEnabled(getContext()) ?
                         getString(R.string.advanced_battery_preference_summary_with_hours) :
                         getString(R.string.advanced_battery_preference_summary));
 
